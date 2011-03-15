dfc6d68e2e71c7a94b49f18e95a1287669dbb6d2
Will Maier <willmaier@ml1.net>
2011-03-15 09:27:02 -0500

    Add a setup.py.

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+options = dict(
+    name="taskmaster",
+    version = "0.1",
+    description="run tasks in parallel",
+    author="Will Maier",
+    author_email="willmaier@ml1.net",
+    py_modules=["taskmaster"],
+    test_suite="tests",
+    use_2to3=True,
+    keywords="parallel ssh distributed management",
+    url="http://packages.python.org/taskmaster",
+    classifiers = [
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: POSIX",
+        "Natural Language :: English",
+        "Programming Language :: Python",
+        "#Programming Language :: Python :: 3",
+        "Topic :: System :: Distributed Computing",
+        "Topic :: System :: Installation/Setup",
+        "Topic :: System :: Software Distribution",
+        "Topic :: Utilities",
+    ],
+)
+
+setup(**options)
